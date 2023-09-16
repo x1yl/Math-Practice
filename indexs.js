2d517031e344f3633f354f483a0ee73727f6f43f
x1yl <life23243@tutanota.com>
2023-09-16 16:52:19 -0400

    Added a dedicated times table practice

---
+++ indexs.js
@@ -0,0 +1,55 @@
+let correctAnswer;
+
+function generateEquation() {
+  const operation = 'multiplication';
+  const digits = '2';
+  const maxNumber = "12";
+
+  const randomNumber = () => Math.floor(Math.random() * maxNumber) + 1;
+
+  let equation = '';
+  correctAnswer = 0;
+
+  for (let i = 0; i < digits; i++) {
+    const operand = randomNumber();
+    equation += `${operand} `;
+    correctAnswer = calculateCorrectAnswer(operation, equation);
+    if (i < digits - 1) {
+      if (operation === 'addition') equation += '+ ';
+      else if (operation === 'subtraction') equation += '- ';
+      else if (operation === 'multiplication') equation += '× ';
+      else if (operation === 'division') equation += '÷ ';
+    }
+  }
+  
+  document.getElementById('equation').innerText = equation;
+}
+
+function calculateCorrectAnswer(operation, equation) {
+  if (operation === 'addition') return eval(equation);
+  else if (operation === 'subtraction') return eval(equation);
+  else if (operation === 'multiplication') {
+    equation = equation.replace(/×/g, '*')
+    return eval(equation)
+  }
+  else if (operation === 'division') {
+    equation = equation.replace(/÷/g, '/')
+    return eval(equation)
+  };
+}
+
+function checkAnswer() {
+  const userAnswer = parseFloat(document.getElementById('answer').value, 10);
+  if (userAnswer == parseFloat(correctAnswer.toFixed(2))) {
+    document.getElementById('result').innerText = 'Correct!';
+  } else {
+    document.getElementById('result').innerText = 'Incorrect. Try again!';
+  }
+}
+
+function handleKeyPress(event) {
+    if (event.key === 'Enter') {
+      checkAnswer();
+    }
+  }
+  generateEquation()
