d6b622c46015c25e42c9d215cee727da91b170bf
x1yl <life23243@tutanota.com>
2023-09-16 18:15:16 -0400

    stuff

--- indexs.js
+++ indexs.js
@@ -1,7 +1,6 @@
 let correctAnswer;
 
 function generateEquation() {
-  const operation = 'multiplication';
   const digits = '2';
   const maxNumber = "12";
 
@@ -13,34 +12,23 @@ function generateEquation() {
   for (let i = 0; i < digits; i++) {
     const operand = randomNumber();
     equation += `${operand} `;
-    correctAnswer = calculateCorrectAnswer(operation, equation);
+    correctAnswer = calculateCorrectAnswer(equation);
     if (i < digits - 1) {
-      if (operation === 'addition') equation += '+ ';
-      else if (operation === 'subtraction') equation += '- ';
-      else if (operation === 'multiplication') equation += '× ';
-      else if (operation === 'division') equation += '÷ ';
+      equation += '× ';
     }
   }
   
   document.getElementById('equation').innerText = equation;
 }
 
-function calculateCorrectAnswer(operation, equation) {
-  if (operation === 'addition') return eval(equation);
-  else if (operation === 'subtraction') return eval(equation);
-  else if (operation === 'multiplication') {
+function calculateCorrectAnswer(equation) {
     equation = equation.replace(/×/g, '*')
     return eval(equation)
-  }
-  else if (operation === 'division') {
-    equation = equation.replace(/÷/g, '/')
-    return eval(equation)
-  };
 }
 
 function checkAnswer() {
   const userAnswer = parseFloat(document.getElementById('answer').value, 10);
-  if (userAnswer == parseFloat(correctAnswer.toFixed(2))) {
+  if (userAnswer == parseFloat(correctAnswer)) {
     document.getElementById('result').innerText = 'Correct!';
   } else {
     document.getElementById('result').innerText = 'Incorrect. Try again!';
